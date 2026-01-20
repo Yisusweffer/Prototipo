9a8405a1369ec4c789ac977678a27a554c9daad6
OsbethCo <mamelillo849@gmail.com>
2026-01-20 11:58:04 -0400

    correccion

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,4 +2,17 @@ const express = require('express');
 const cors = require('cors');
 
 const authRoutes = require('./routes/auth.routes');
-const
+const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/usuarios', userRoutes);
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
